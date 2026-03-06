6cd638e065a84599511922a5fa5822c73e283046
balongie <butiubalong@gmail.com>
2026-03-06 14:30:15 +0800

    models.py

--- src/database/models.py
+++ src/database/models.py
@@ -35,56 +35,26 @@ class Task(Base):
 class User(Base):
     __tablename__ = "User"
     id: Mapped[int] = mapped_column(primary_key=True)
+    firstname: Mapped[str]
+    lastname: Mapped[str]
     username: Mapped[str]
     email: Mapped[str]
     hash: Mapped[str]
     role: Mapped[str]
     is_deleted: Mapped[bool] = mapped_column(nullable=True, server_default="False")
 
-
-    tasks: Mapped[List["TaskAssignee"]] = relationship(back_populates="user", passive_deletes=True)
-
-class TaskAssignee(Base):
-    __tablename__ = "TaskAssignee"
+class Post(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
-    taskID: Mapped[int] = mapped_column(ForeignKey("Task.id", ondelete="CASCADE"))
-    assigneeID: Mapped[int] = mapped_column(ForeignKey("User.id", ondelete="CASCADE"))
+    user_id: Mapped[int] = mapped_column(ForeignKey("User.id", ondelete="CASCADE"))
+    title: Mapped[str]
+    description: Mapped[str] = mapped_column(nullable=True)
+    attachment: Mapped[str] = mapped_column(nullable=True)
 
-    task: Mapped["Task"] = relationship(back_populates="assignees")
-    user: Mapped["User"] = relationship(back_populates="tasks")
-
-    comments: Mapped[List["Comment"]] = relationship(back_populates="taskAssignee")
-
-
-class Comment(Base):
-    __tablename__ = "Comment"
-    id: Mapped[int] = mapped_column(primary_key=True)
-    taskAssigneeID: Mapped[int] =  mapped_column(ForeignKey("TaskAssignee.id", ondelete="CASCADE"))
-    replyerID: Mapped[int] = mapped_column(ForeignKey("User.id", ondelete="CASCADE"), nullable=True)
-    date: Mapped[datetime.datetime] = mapped_column(server_default=func.now())
-    reply: Mapped[str] = mapped_column(nullable=True)
-    comment: Mapped[str] = mapped_column(nullable=True)
-
-    taskAssignee: Mapped["TaskAssignee"] = relationship(back_populates="comments")
-    replyer: Mapped["User"] = relationship()
+class Comments(Base):
+    post_id: Mapped[int] = mapped_column(ForeignKey("Post.id", ondelete="CASCADE"), primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("User.id", ondelete="CASCADE"), primary_key=True)
+    comment: Mapped[str]
+    date_created: Mapped[str]
 
 
-class TaskAttachment(Base):
-    __tablename__ = "TaskAttachment"
-    id: Mapped[int] = mapped_column(primary_key=True)
-    taskID: Mapped[int] = mapped_column(ForeignKey("Task.id", ondelete="CASCADE"))
-    fileURL: Mapped[str] = mapped_column(nullable=True)
-
-    task: Mapped["Task"] = relationship(back_populates="attachments")
-
-
-class Log(Base):
-    __tablename__ = "Log"
-    id: Mapped[int] = mapped_column(primary_key=True)
-    taskID: Mapped[int] = mapped_column(ForeignKey("Task.id", ondelete="SET NULL"), nullable=True)
-    current_userID: Mapped[int] = mapped_column(ForeignKey("User.id", ondelete="SET NULL"), nullable=True)
-    affected_userID: Mapped[int] = mapped_column(ForeignKey("User.id", ondelete="SET NULL"), nullable=True)
-    username: Mapped[str] = mapped_column(nullable=True)
-    date: Mapped[datetime.datetime] = mapped_column(nullable=True, server_default=func.now())
-    type: Mapped[str]
-    viewed: Mapped[bool] = mapped_column(default=False)
+    
